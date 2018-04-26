caaaa448ad9377d8fe52cfc416f1037f16edeeb7
Cyrill Troxler <cyrilltroxler@gmail.com>
2018-04-26 20:33:36 +0200

    Initial commit

---
+++ GetUrl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace GetUrl
+{
+    [Cmdlet(VerbsCommon.Get, "Url")]
+    public class GetUrlCommand : PSCmdlet
+    {
+        [Parameter(Position=1)]
+        public string Message { get; set; } = string.Empty;
+
+        protected override void EndProcessing()
+        {
+
+            var HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://google.ch");
+            client.DefaultRequestHeaders.Accept.Clear();
+            try {
+              HttpResponseMessage response = client.GetAsync("/").Wait();
+              if (response.IsSuccessStatusCode)
+              {
+                this.WriteObject(response);
+              }
+            }
+
+            base.EndProcessing();
+        }
+    }
+}
