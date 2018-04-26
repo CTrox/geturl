54562eb4dc235c70903d75d1d27a4f0ef424d162
Noël Widmer <noel.widmer@gmail.com>
2018-04-26 20:46:39 +0200

    Fixed project.

---
+++ GetUrl/GetUrlCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Management.Automation;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace GetUrl
+{
+    [Cmdlet(VerbsCommon.Get, "Url")]
+    public class GetUrlCommand : PSCmdlet
+    {
+        [Parameter(Position = 1)]
+        public string Message { get; set; } = string.Empty;
+
+        protected override void EndProcessing()
+        {
+
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("http://google.ch");
+            client.DefaultRequestHeaders.Accept.Clear();
+            try
+            {
+                var task = client.GetAsync("/");
+                task.Wait();
+
+                var response = task.Result;
+                if(response.IsSuccessStatusCode)
+                {
+                    WriteObject(response);
+                }
+            }
+            catch(Exception e)
+            {
+                this.WriteObject(e.Message);
+            }
+
+            base.EndProcessing();
+        }
+    }
+}
